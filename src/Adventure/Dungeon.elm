dffc2c396e5146797cb33daa97982ee6569d9314
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2017-05-19 22:21:32 +0200

    hmm

--- src/Adventure/Dungeon.elm
+++ src/Adventure/Dungeon.elm
@@ -1,14 +1,22 @@
 module Adventure.Dungeon exposing (..)
 
+import Maybe exposing (Maybe(..))
+import Adventure.Room exposing (Room)
 import Adventure.Rooms.DankCellar exposing (dankcellar)
 import Adventure.Rooms.DiningHall exposing (dininghall)
 
 
+type alias Dungeon =
+    { name : String
+    , map : List (List (Maybe Room))
+    }
+
+
+dungeon : Dungeon
 dungeon =
-    { room = dankcellar
-    , exits =
-        [ { room = dininghall
-          , exitdescription = "You see a feeble ladder leading upp towards a dim light."
-          }
+    { name = "Your standard dungeon"
+    , map =
+        [ [ Just dininghall ]
+        , [ Just dankcellar ]
         ]
     }
